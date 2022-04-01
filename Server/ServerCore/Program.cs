e68363e322bfac89b79b29827d6ab6b9054fa0a9
jangjo123 <cjkh1324@naver.com>
2022-04-01 23:31:39 +0900

    Thread

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Threading;
 
 namespace ServerCore
 {
     class Program
     {
+        static void MainThread(object state)
+        {
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine("Hello Thread!");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ThreadPool.QueueUserWorkItem(MainThread);
+
+            //Thread t = new Thread(MainThread);
+            //t.Name = "Test Thread";
+            //t.IsBackground = true;
+            //t.Start();
+
+            //Console.WriteLine("Waiting for Thread!");
+
+            //t.Join();
+            //Console.WriteLine("Hello World!");
+            while (true)
+            {
+
+            }
         }
     }
 }
