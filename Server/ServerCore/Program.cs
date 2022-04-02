8cf80bb3752f58955e76d53bd5b8d9435c61d101
jangjo123 <cjkh1324@naver.com>
2022-04-02 18:51:15 +0900

    Lock

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -7,31 +7,39 @@ namespace ServerCore
     class Program
     {
         static int number = 0;
+        static object _obj = new object();
 
         static void Thread_1()
         {
-            // atomic  = 원자성
-
-            // 검 구매 과정
-            // 골드 -= 100;
-                // 서버 다운(골드는 줄고 검은 안사짐)
-            // 인벤 += 검
-            
-            // 집행검 User2 인벤이 넣어라 - Ok
-            // 집행검 User1 인벤에서 없애라 - fail (아이템 복사 문제)
 
             for (int i = 0; i < 100000; i++)
             {
-                // ref를 넣는 이유: number를 참조해야 원자적이게 됨. 
+                // 상호배제 Mutual Exclusive
+                lock (_obj) // try, finally 예시와 비슷하게 동작함
+                {
+                    number++;
+                }
 
-                int affterValue = Interlocked.Increment(ref number); // 원자적으로 이뤄지게 보정 (성능 손해)
+                //try 
+                //{
+                //    Monitor.Enter(_obj);
+                //    number++;
 
-                //number++;
+                //    return;
+                //}
+                //finally // 데드락 해결
+                //{
+                //    Monitor.Exit(_obj);
+                //}
 
-                // 어셈블리에서 number++ 이 돌아가는 과정 느낌
-                //int temp = number; // 0
-                //temp += 1; // 1
-                //number = temp; // number = 1
+                //Monitor.Enter(_obj); // 문을 잠구는 행위
+
+                //{
+                //    number++;
+                //    return; // 문을 안열고 나감.. (데드락, DeadLock)
+                //}
+
+                //Monitor.Exit(_obj); // 잠금을 풀어준다.
             }
         }
 
@@ -39,9 +47,10 @@ namespace ServerCore
         {
             for (int i = 0; i < 100000; i++)
             {
-                Interlocked.Decrement(ref number);
-
-                // number--;
+                lock (_obj) // try, finally 예시와 비슷하게 동작함
+                {
+                    number--;
+                }
             }
         }
 
