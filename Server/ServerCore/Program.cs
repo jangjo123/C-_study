c8d70491993ca20ad7fb0480cf8cc1443e72331d
jangjo123 <cjkh1324@naver.com>
2022-04-02 16:15:35 +0900

    Memory Barrier Ex

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -14,54 +14,31 @@ namespace ServerCore
 
     class Program
     {
-        static int x = 0;
-        static int y = 0;
-        static int r1 = 0;
-        static int r2 = 0;
+        int _answer;
+        bool _complete;
 
-        static void Thread_1()
+        void A()
         {
-            y = 1; // Store y
-
-            // ---------------------------
-            Thread.MemoryBarrier();
-
-            r1 = x; // Load x
+            _answer = 123;
+            Thread.MemoryBarrier(); // Barrier 1
+            _complete = true;
+            Thread.MemoryBarrier(); // Barrier 2
         }
 
-        static void Thread_2()
+        void B()
         {
-            x = 1; // Store x
-
-            // ---------------------------
-            Thread.MemoryBarrier();
-
-            r2 = y; // Load y
+            Thread.MemoryBarrier(); // Barrier 3
+            if (_complete)
+            {
+                Thread.MemoryBarrier(); // Barrier 4
+                Console.WriteLine(_answer);
+            }
         }
-
 
 
         static void Main(string[] args)
         {
-            int count = 0;
-            while (true)
-            {
-                count++;
-                x = y = r1 = r2 = 0;
 
-                Task t1 = new Task(Thread_1);
-                Task t2 = new Task(Thread_2);
-                t1.Start();
-                t2.Start();
-
-                Task.WaitAll(t1, t2);
-
-                if (r1 == 0 && r2 == 0)
-                    break;
-
-            }
-
-            Console.WriteLine($"{count}번만에 빠져나옴!");
         }
     }
 }
