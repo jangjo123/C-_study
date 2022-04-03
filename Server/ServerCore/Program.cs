b259d4442cdce25f449513c33d06a2c3d80bfdaa
jangjo123 <cjkh1324@naver.com>
2022-04-03 20:49:24 +0900

    lock 구현 정리

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -7,45 +7,36 @@ namespace ServerCore
 
     class Program
     {
-        static int _num = 0;
+        // 1. 근성
+        // 2. 양보
+        // 3. 갑질
 
-        // AutoResetEvent는 bool 정보만.. (잠금 유무)
+        // 상호배제
+        // Monitor
+        static object _lock = new object(); 
+        static SpinLock _lock2 = new SpinLock(); // 너무 안되면 잠깐 yield
+        // 직접 만든다.
 
-        // int (몇개 잠궜는지) ThreadID
-        static Mutex _lock = new Mutex(); // 커널 동기화 객체
-
-        static void Thread_1()
-        {
-            for (int i = 0; i < 100000; i++)
-            {
-                _lock.WaitOne();
-                _lock.WaitOne();
-                _num++;
-                _lock.ReleaseMutex();
-                _lock.ReleaseMutex();
-            }
-        }
-
-        static void Thread_2()
-        {
-            for (int i = 0; i < 100000; i++)
-            {
-                _lock.WaitOne();
-                _num--;
-                _lock.ReleaseMutex();
-            }
-        }
+        
 
         static void Main(string[] args)
         {
-            Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
-            t1.Start();
-            t2.Start();
+            lock (_lock)
+            {
 
-            Task.WaitAll(t1, t2);
+            }
 
-            Console.WriteLine(_num);
+            bool lockTaken = false; // 결과물
+            try
+            {
+                _lock2.Enter(ref lockTaken);
+            }
+            finally
+            {
+                if (lockTaken)
+                    _lock2.Exit();
+            }
+
         }
     }
 }
