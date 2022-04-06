10915ee565b6881054978fb9da2e17760494022f
jangjo123 <cjkh1324@naver.com>
2022-04-06 22:49:48 +0900

    socket + server

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,36 +1,64 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    // [ JobQueue ] -> 일이 있을 때 락을 걸고 최대한 TLS에 옮기고 거기서 일을함. (ThreadLocal)
-
     class Program
     {
-        // 쓰레드 마다 고유하게 갖는 값                                       // ThreadName이 null 일때 여기서 만듬..
-        static ThreadLocal<string> ThreadName = new ThreadLocal<string>(() => { return $"My Name is {Thread.CurrentThread.ManagedThreadId}"; }); 
-        // static string ThreadName // Program.cs 안에 있는 쓰레드들이 모두 공유
-
-        static void WhoAmI()
-        {
-            bool repeat = ThreadName.IsValueCreated;
-            if(repeat)
-                Console.WriteLine(ThreadName.Value + " (repeat)");
-
-            else
-                Console.WriteLine(ThreadName.Value);
-        }
-
         static void Main(string[] args)
         {
+            // DNS (Domain Name System) // www.naver.com -> 123.123.123.12
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(3, 3);
+            // 문지기
+            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
-            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
+            try
+            {
+                // 문지기 교육
+                listenSocket.Bind(endPoint);
 
-            ThreadName.Dispose(); // 지우기
+                // 영업 시작
+                // backlog : 최대 대기수 (Listen(backlog))
+                listenSocket.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Listening....");
+
+                    // 손님을 입장 시킨다.. + 대리인 생성
+                    Socket clientSocket = listenSocket.Accept();
+
+                    // 손님이 말한 것을 저장하는 곳
+                    byte[] reveBuff = new byte[1024];
+
+                    // 손님이 말한 것을 받는다 (몇 바이트 받는지는 따로 저장)
+                    int recvBytes = clientSocket.Receive(reveBuff);
+                    string recvData = Encoding.UTF8.GetString(reveBuff, 0, recvBytes);
+                    Console.WriteLine($"[From Client] {recvData}");
+
+                    // 보낸다
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to Server !");
+                    clientSocket.Send(sendBuff);
+
+                    // 쫒아낸다.
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            
         }
     }
 }
