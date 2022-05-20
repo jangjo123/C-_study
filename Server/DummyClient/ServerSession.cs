5e27bbf5e9cc209f241ce6e361acec14e54bbeff
jangjo123 <cjkh1324@naver.com>
2022-05-20 10:42:49 +0900

    Packet Generator, xml파일 생성, PacketFormat 생성

--- Server/DummyClient/ServerSession.cs
+++ Server/DummyClient/ServerSession.cs
@@ -8,16 +8,8 @@ using ServerCore;
 
 namespace DummyClient
 {
-    public abstract class Packet
-    {
-        public ushort size;
-        public ushort packetid;
 
-        public abstract ArraySegment<byte> Write();
-        public abstract void Read(ArraySegment<byte> s);
-    }
-
-    class PlayerInfoReq : Packet
+    class PlayerInfoReq
     {
         public long playerId;
         public string name;
@@ -54,12 +46,7 @@ namespace DummyClient
 
         public List<SkillInfo> skills = new List<SkillInfo>();
 
-        public PlayerInfoReq()
-        {
-            this.packetid = (ushort)PacketID.PlayerInfoReq;
-        }
-
-        public override void Read(ArraySegment<byte> segment)
+        public void Read(ArraySegment<byte> segment)
         {
             ushort count = 0;
 
@@ -88,7 +75,7 @@ namespace DummyClient
             }
         }
 
-        public override ArraySegment<byte> Write()
+        public ArraySegment<byte> Write()
         {
             ArraySegment<byte> segment = SendBufferHelper.Open(4096);
 
@@ -98,7 +85,7 @@ namespace DummyClient
             Span<byte> s = new Span<byte>(segment.Array, segment.Offset, segment.Count);
 
             count += sizeof(ushort);
-            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), this.packetid);
+            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), (ushort)PacketID.PlayerInfoReq);
             count += sizeof(ushort);
             success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), this.playerId);
             count += sizeof(long);
---
+++ Server/PacketGenerator/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Xml;
+
+namespace PacketGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlReaderSettings settings = new XmlReaderSettings()
+            {
+                IgnoreComments = true, // 주석 무시
+                IgnoreWhitespace = true // 공백 무시
+            };
+
+            using (XmlReader r = XmlReader.Create("PDL.xml", settings))
+            {
+                r.MoveToContent(); // 헤더 건너뛰기 -> <?xml version="1.0" encoding="utf-8" ?> <PDL> 를 건너 뜀
+
+                while (r.Read())
+                {
+                    if (r.Depth == 1 && r.NodeType == XmlNodeType.Element) // r.Name이 packet이라면 (tab이 하나라면) and 같은 Depth중 처음꺼라면
+                        ParsePacket(r);
+                    // Console.WriteLine(r.Name + " " + r["name"]); // r.Name -> type, r["name"] -> name="여기값"
+                }
+            }
+
+            
+        }
+
+        public static void ParsePacket(XmlReader r)
+        {
+            if (r.NodeType == XmlNodeType.EndElement)
+                return;
+
+            if (r.Name.ToLower() != "packet")
+            {
+                Console.WriteLine("invalid packet node");
+                return;
+            }
+
+            string packetName = r["name"];
+            if (string.IsNullOrEmpty(packetName))
+            {
+                Console.WriteLine("Packet without name");
+                return;
+            }
+
+            ParseMembers(r);
+
+        }
+
+        public static void ParseMembers(XmlReader r)
+        {
+            string packetName = r["name"];
+
+            int depth = r.Depth + 1;
+            while (r.Read())
+            {
+                if (r.Depth != depth)
+                    break;
+
+                string memberName = r["name"];
+                if (string.IsNullOrEmpty(memberName))
+                {
+                    Console.WriteLine("Member without name");
+                    return;
+                }
+
+                string memberType = r.Name.ToLower();
+                switch (memberType)
+                {
+                    case "bool":
+                    case "byte":
+                    case "short":
+                    case "ushort":
+                    case "int":
+                    case "long":
+                    case "float":
+                    case "double":
+                    case "string":
+                    case "list":
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
