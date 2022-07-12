41708335c6a9ffd5b0d6e4efbb2dc817ece4d33f
jangjo123 <cjkh1324@naver.com>
2022-07-12 19:30:31 +0900

    State 관리

---
+++ WebServer/BlazorApp/BlazorStudy2/Data/CounterState.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorStudy2.Data
+{
+    public class CounterState
+    {
+        int _count = 0;
+
+        public Action OnStateChanged;
+
+        public int Count
+        {
+            get 
+            {
+                return _count;
+            }
+
+            set 
+            {
+                _count = value;
+                Refresh();
+            }
+        }
+
+        void Refresh()
+        {
+            OnStateChanged?.Invoke();
+        }
+    }
+}
