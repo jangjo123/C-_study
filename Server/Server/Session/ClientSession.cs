88246676ffe4c8d2a1b12b6c6dd948fa89f701c8
jangjo123 <cjkh1324@naver.com>
2022-07-29 20:54:12 +0900

    Protobuf 29분

--- Server/Server/Session/ClientSession.cs
+++ Server/Server/Session/ClientSession.cs
@@ -6,13 +6,16 @@ using System.Threading;
 using System.Threading.Tasks;
 using ServerCore;
 using System.Net;
+using Google.Protobuf.Protocol;
+using static Google.Protobuf.Protocol.Person.Types;
+using Google.Protobuf;
 
 namespace Server
 {
 	class ClientSession : PacketSession
 	{
 		public int SessionId { get; set; }
-		public GameRoom Room { get; set; }
+		//public GameRoom Room { get; set; }
 		public float PosX { get; set; }
 		public float PosY { get; set; }
 		public float PosZ { get; set; }
@@ -21,23 +24,34 @@ namespace Server
 		{
 			Console.WriteLine($"OnConnected : {endPoint}");
 
-			Program.Room.Push(() => Program.Room.Enter(this));
+			Person person = new Person()
+			{
+				Name = "Gunal",
+				Id = 123,
+				Email = "Gunal@naver.com",
+				Phones = { new PhoneNumber { Number = "555-4321", Type = PhoneType.Home } }
+			};
+
+			int size = (ushort)person.CalculateSize();
+			byte[] sendBuffer = new byte[size + 4];
+			Array.Copy(BitConverter.GetBytes(size + 4), 0, sendBuffer, 0, sizeof(ushort));
+			ushort protocolId = 1;
+			Array.Copy(BitConverter.GetBytes(protocolId), 0, sendBuffer, 2, sizeof(ushort));
+			Array.Copy(person.ToByteArray(), 0, sendBuffer, 4, size);
+
+			Send(new ArraySegment<byte>(sendBuffer));
+
+			//Program.Room.Push(() => Program.Room.Enter(this));
 		}
 
 		public override void OnRecvPacket(ArraySegment<byte> buffer)
 		{
-			PacketManager.Instance.OnRecvPacket(this, buffer);
+			//PacketManager.Instance.OnRecvPacket(this, buffer);
 		}
 
 		public override void OnDisconnected(EndPoint endPoint)
 		{
 			SessionManager.Instance.Remove(this);
-			if (Room != null)
-			{
-				GameRoom room = Room;
-				room.Push(() => room.Leave(this));
-				Room = null;
-			}
 
 			Console.WriteLine($"OnDisconnected : {endPoint}");
 		}
