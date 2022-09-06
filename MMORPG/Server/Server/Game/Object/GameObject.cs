d272808897ccb8f203feb0505ea81218b267ef54
jangjo123 <cjkh1324@naver.com>
2022-09-06 16:50:59 +0900

    Stat

--- MMORPG/Server/Server/Game/Object/GameObject.cs
+++ MMORPG/Server/Server/Game/Object/GameObject.cs
@@ -18,11 +18,18 @@ namespace Server.Game
 
         public ObjectInfo Info { get; set; } = new ObjectInfo();
         public PositionInfo PosInfo { get; private set; } = new PositionInfo();
+		public StatInfo Stat { get; private set; } = new StatInfo();
 
+		public float Speed
+        {
+			get { return Stat.Speed; }
+			set { Stat.Speed = value; }
+        }
 
         public GameObject()
         {
             Info.PosInfo = PosInfo;
+			Info.StatInfo = Stat;
         }
 
 		public Vector2Int CellPos
@@ -66,5 +73,10 @@ namespace Server.Game
 
 			return cellPos;
 		}
+
+		public virtual void OnDamaged(GameObject attacker, int damage)
+        {
+
+        }
 	}
 }
